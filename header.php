c77351259faf721f0b2e1656c367ba2d46890316
Moïse Kaseka <mokadev21@gmail.com>
2022-04-26 16:02:26 -0400

    s12-c12-2 Boîte moodale complété

--- header.php
+++ header.php
@@ -9,7 +9,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Montserrat+Alternates:wght@300&family=Saira:wght@100&family=Work+Sans&display=swap" rel="stylesheet">
     <?php wp_head() ?>
     <?php show_admin_bar(true); ?>
 </head>
