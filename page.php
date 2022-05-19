2d8d9f61bb662af3e660e326d33cd7f8111326fe
Moïse Kaseka <mokadev21@gmail.com>
2022-05-19 14:55:23 -0400

    l'ajout des champs persnoliser de pages d'atelier

--- page.php
+++ page.php
@@ -1,9 +1,19 @@
 <?php get_header() ?>
 <main class="site__main">
-
    <?php if (have_posts()): the_post(); ?>
-        <?php the_title() ?>
-        <?php the_content() ?>        
+      <h1><?php the_title() ?></h1>
+      <?php the_content() ?>
+      <h1 class="atelier__local">L'atelier: <?= get_field( "duree" ); ?></h1>
+      <h3>Description de l'atelier</h3>
+      <p class="atelier__dep">L'animateur du cours: <?= get_field( "animateur" ); ?></p>   
+      <p class="atelier__local">L'atelier sera donné au local: <?= get_field( "local" ); ?></p>
+      <p class="atelier__desc"><?= get_field( "desc" ); ?></p> 
+      <h3>Horaire et dates de l'atelier</h3>
+      <p class="atelier__duree">Durée de chacune des séance est de: <?= get_field( "duree" ); ?> heures</p>
+      <p class="atelier__debut">Date de début: <?= get_field( "debut" ); ?></p> 
+      <p class="atelier__fin">Date de fin: <?= get_field( "fin" ); ?></p>
+      <p class="atelier__jour">La formation se donnera: <?= get_field( "jourdeform" ); ?></p>
+      <p class="atelier__horaire">L'horaire: <?= get_field( "hdebut" ); ?> à <?= get_field( "hfin" ); ?></p>         
    <?php endif ?>
 </main>
 <?php get_footer() ?>
