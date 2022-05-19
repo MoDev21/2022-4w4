afc3f9ad53bc3e331ffb06e89802ec2369e9aa72
Moïse Kaseka <mokadev21@gmail.com>
2022-05-19 15:14:52 -0400

    Amélioration des champs pers0naliser de pages d'atelier

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
    <?php if (have_posts()): the_post(); ?>
       <h1><?php the_title() ?></h1>
       <?php the_content() ?>
-      <h1 class="atelier__local">L'atelier: <?= get_field( "duree" ); ?></h1>
+      <h1 class="atelier__local">L'atelier: <?php the_title() ?></h1>
       <h3>Description de l'atelier</h3>
       <p class="atelier__dep">L'animateur du cours: <?= get_field( "animateur" ); ?></p>   
       <p class="atelier__local">L'atelier sera donné au local: <?= get_field( "local" ); ?></p>
@@ -12,7 +12,7 @@
       <p class="atelier__duree">Durée de chacune des séance est de: <?= get_field( "duree" ); ?> heures</p>
       <p class="atelier__debut">Date de début: <?= get_field( "debut" ); ?></p> 
       <p class="atelier__fin">Date de fin: <?= get_field( "fin" ); ?></p>
-      <p class="atelier__jour">La formation se donnera: <?= get_field( "jourdeform" ); ?></p>
+      <p class="atelier__jour">La formation se donnera: <?= get_field( "jour" ); ?></p>
       <p class="atelier__horaire">L'horaire: <?= get_field( "hdebut" ); ?> à <?= get_field( "hfin" ); ?></p>         
    <?php endif ?>
 </main>
